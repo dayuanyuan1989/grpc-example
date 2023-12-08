bfee8cbfeed8de534866bbf3f52473c1a59ad2de
cuiyuanyuan <cuiyuanyuan@senseauto.com>
2023-12-08 19:48:10 +0800

    分离客户端发送和接收逻辑

--- client_async.py
+++ client_async.py
@@ -2,17 +2,42 @@ import asyncio
 import grpc
 import chat_pb2
 import chat_pb2_grpc
+import threading
+from queue import Queue
+
+stream = None
+sync_queue = Queue()
+
+def loop_write():
+    index = 1
+    while True:
+        write_to_sync_queue(f"client-{index}")
+        index += 1
+
+
+def write_to_sync_queue(data):
+    global sync_queue
+    sync_queue.put(data)
 
 async def chat(client):
-    # async with client.JoinChat() as stream:
     stream = client.JoinChat()
-    # Send messages
-    await stream.write(chat_pb2.ChatMessage(text="Hello"))
-    await stream.write(chat_pb2.ChatMessage(text="Async"))
+
+    send_task = asyncio.create_task(send(stream))
 
     # Receive responses
     async for response in stream:
         print(f"Received response: {response.text}")
+
+
+async def send(stream):
+    global sync_queue
+
+    while True:
+        data = await asyncio.get_event_loop().run_in_executor(None, sync_queue.get)
+        if data is None:
+            continue
+        await stream.write(chat_pb2.ChatMessage(text=data))
+
 
 async def run_client():
     async with grpc.aio.insecure_channel('localhost:5501') as channel:
@@ -20,4 +45,7 @@ async def run_client():
         await chat(client)
 
 if __name__ == '__main__':
+    t = threading.Thread(target=loop_write)
+    t.start()
     asyncio.run(run_client())
+    t.join()
