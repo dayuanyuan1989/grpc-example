8076752797c19b440aa2c24d5090386aa0a0cf44
cuiyuanyuan <cuiyuanyuan@senseauto.com>
2023-12-29 10:50:15 +0800

    add queue in server_async

--- server_async.py
+++ server_async.py
@@ -2,16 +2,41 @@ import grpc
 import chat_pb2
 import chat_pb2_grpc
 import asyncio
+import threading
+from queue import Queue
+import time
+
+sync_queue = Queue()
+
+def loop_write():
+    index = 1
+    while True:
+        write_to_sync_queue(f"server-{index}")
+        index += 1
+
+def write_to_sync_queue(data):
+    global sync_queue
+    sync_queue.put(data)
+
 
 class ChatService(chat_pb2_grpc.ChatServicer):
     async def JoinChat(self, request_iterator, context: grpc.ServicerContext):
+        send_task = asyncio.create_task(self.send(context))
+
         # Iterate over incoming messages asynchronously
         async for request in request_iterator:
             # Handle incoming messages asynchronously
             print(f"Received message: {request.text}")
 
+    async def send(self, context):
+        global sync_queue
+
+        while True:
+            data = await asyncio.get_event_loop().run_in_executor(None, sync_queue.get)
+            if data is None:
+                continue
             # Send a response asynchronously
-            response = chat_pb2.ChatMessage(text=f"Server: Hello, {request.text}!")
+            response = chat_pb2.ChatMessage(text=f"Server: Hello, {data}!")
             await self.send_message_to_client(response, context)
 
     async def send_message_to_client(self, message, context: grpc.ServicerContext):
@@ -26,4 +51,7 @@ async def serve():
     await server.wait_for_termination()
 
 if __name__ == '__main__':
+    t = threading.Thread(target=loop_write)
+    t.start()
     asyncio.run(serve())
+    t.join()
